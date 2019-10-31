daeedeb5d6454816150f6f93b149ef2639979111
Brian <bri2210433@yahoo.com>
2019-10-30 19:22:00 -0700

    all but breaks

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ancr_tags = document.querySelectorAll('nav a');
+ancr_tags[0].textContent = 'Services';
+ancr_tags[1].textContent = 'Products';
+ancr_tags[2].textContent = 'Vision';
+ancr_tags[3].textContent = 'Features';
+ancr_tags[4].textContent = 'About';
+ancr_tags[5].textContent = 'Contact';
+
+//Header
+
+let head_img = document.querySelector('#cta-img');
+head_img.setAttribute('src', 'img/header-img.png');
+
+let head_text = document.querySelector('h1');
+head_text.textContent = 'DOM \n IS \n AWESOME';
+
+let button_txt = document.querySelector('button');
+button_txt.textContent = "Get Started";
+
+//Section
+
+let top_lft_h = document.querySelectorAll('.main-content .text-content h4');
+top_lft_h[0].textContent = "Features";
+top_lft_h[1].textContent = "About";
+top_lft_h[2].textContent = "Services"
+top_lft_h[3].textContent = "Product";
+top_lft_h[4].textContent = "Vision";
+
+let top_lft_txt = document.querySelectorAll('.main-content .text-content p');
+top_lft_txt[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+top_lft_txt[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+top_lft_txt[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+top_lft_txt[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+top_lft_txt[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+let mid_img = document.querySelector('.middle-img')
+mid_img.setAttribute('src', 'img/mid-page-accent.jpg');
+
+
+//Contact
+
+let contact_header = document.querySelector('.contact h4');
+contact_header.textContent = 'Contact';
+
+let contact_p = document.querySelectorAll('.contact p');
+contact_p[0].textContent = "123 Way 456 Street \n Somewhere, USA";
+
+contact_p[1].textContent = "1 (888) 888-8888";
+
+contact_p[2].textContent = "sales@greatidea.io";
+
+//Footer
+
+let footer_p = document.querySelector('footer p');
+footer_p.textContent = "Copyright Great Idea! 2018";
